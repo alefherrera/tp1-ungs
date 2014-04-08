ad0e1ffde8ea7cdb89f84161f4ebc9fb3b9bc6d3
Alejandro <alejandro.herrera@targetamericas.com>
2014-04-08 12:45:59 -0300

    codigo base para el juego

---
+++ TP1/src/juego/SyE.java
@@ -0,0 +1,33 @@
+package juego;
+
+import java.util.List;
+
+public class SyE {
+
+	public List<String> jugadores;
+
+	public SyE(int n) {//tamaño del tablero, donde n > 1
+
+	}
+
+	public String ganador() {//devuelve el nro de jugador ganador o 0 si no hay
+		return "";
+	}
+
+	public void agregarSerpiente(int i, int j) {//caigo de j a i, donde 1 < i < j < n
+
+	}
+
+	public void agregarEscalera(int i, int j) {//subo de i a j, donde 1 < i < j < n
+
+	}
+
+	public void jugar() {//tira los dos dados
+
+	}
+
+	public String ver() {//muestra el estado del tablero y los jugadores
+		return "";
+	}
+
+}
