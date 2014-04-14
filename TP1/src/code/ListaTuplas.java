916fc0731201013ae2a816a559a4508826f4a5e4
Ale <Ale@Rasheem>
2014-04-13 23:06:20 -0300

    quick sort agregado

--- TP1/src/code/ListaTuplas.java
+++ TP1/src/code/ListaTuplas.java
@@ -2,56 +2,52 @@ package code;
 
 import java.util.ArrayList;
 
-public class ListaTuplas<E1,E2> {
+public class ListaTuplas<E1, E2> {
 
-	private ArrayList<Tupla<E1,E2>> lTuplas;
-	
-	public ListaTuplas()
-	{
-		lTuplas = new ArrayList<Tupla<E1,E2>>(); 
+	private ArrayList<Tupla<E1, E2>> lTuplas;
+
+	public ListaTuplas() {
+		lTuplas = new ArrayList<Tupla<E1, E2>>();
 	}
-	
-	public void agregar(Tupla<E1,E2> tupla)
-	{
+
+	public void agregar(Tupla<E1, E2> tupla) {
 		lTuplas.add(tupla);
 	}
 
-	public E2 buscar(E1 elem1){
-		
-		E2 ret=null;
+	public E2 buscar(E1 elem1) {
+
+		E2 ret = null;
 		for (int i = 0; i < lTuplas.size(); i++)
 			if (lTuplas.get(i).getE1().equals(elem1))
 				ret = lTuplas.get(i).getE2();
-		return ret;		
-		
+		return ret;
+
 	}
-	
-	public int tamaño(){
+
+	public int tamaño() {
 		return lTuplas.size();
 	}
 
-	public Tupla<E1,E2> iesimo(int i){
+	public Tupla<E1, E2> iesimo(int i) {
 		return lTuplas.get(i);
 
 	}
-	
-	public void concat(ListaTuplas<E1,E2>... listas)
-	{
-		for(ListaTuplas<E1, E2> arg : listas)
-		{
+
+	public ListaTuplas<E1, E2> concat(ListaTuplas<E1, E2>... listas) {
+		for (ListaTuplas<E1, E2> arg : listas) {
 			for (int i = 0; i < arg.tamaño(); i++) {
 				lTuplas.add(arg.iesimo(i));
 			}
 		}
+		return this;
 	}
-	
+
 	@Override
-	public String toString()
-	{
+	public String toString() {
 		String ret = "";
-		for (int i=0; i<lTuplas.size(); i++)
+		for (int i = 0; i < lTuplas.size(); i++)
 			ret = ret + lTuplas.get(i).toString() + "\n";
 		return ret;
 	}
-	
+
 }
--- TP1/src/code/Promocion.java
+++ TP1/src/code/Promocion.java
@@ -11,6 +11,7 @@ public class Promocion {
 		this.listaPrecios = listaPrecios;
 	}
 
+	// INSERTION SORT
 	ListaTuplas<String, String> resolver_ej1() {
 		// implementar
 		// Se debe pisar listaMateriales.
@@ -46,6 +47,7 @@ public class Promocion {
 		return listaMateriales;
 	}
 
+	// BUBBLE SORT
 	ListaTuplas<String, String> resolver_ej3() {
 		String nombretmp;
 		Integer preciotmp;
@@ -80,6 +82,48 @@ public class Promocion {
 		}
 
 		return listaMateriales;
+	}
+
+	//QUICK SORT
+	ListaTuplas<String, String> resolver_ej1v2() {
+
+		listaPrecios = quicksort(listaPrecios);
+
+		System.out.print(listaPrecios.toString());
+
+		listaMateriales = new ListaTuplas<String, String>();
+		for (int i = 0; i < listaPrecios.tamaño(); i += 2) {
+			listaMateriales.agregar(new Tupla<String, String>(listaPrecios
+					.iesimo(i).getE1(), listaPrecios.iesimo(i + 1).getE1()));
+		}
+
+		return listaMateriales;
+	}
+
+	ListaTuplas<String, Integer> quicksort(ListaTuplas<String, Integer> lista) {
+		System.out.print(lista.tamaño());
+		if (lista.tamaño() <= 1) {
+			return lista;
+		}
+
+		Integer ipivot = (lista.tamaño() - 1) / 2;
+
+		ListaTuplas<String, Integer> menor = new ListaTuplas<String, Integer>();
+		ListaTuplas<String, Integer> mayor = new ListaTuplas<String, Integer>();
+		ListaTuplas<String, Integer> pivot = new ListaTuplas<String, Integer>();
+
+		pivot.agregar(lista.iesimo(ipivot));
+
+		for (int i = 0; i < lista.tamaño(); i++) {
+			if (lista.iesimo(i).getE2() < lista.iesimo(ipivot).getE2()) {
+				menor.agregar(lista.iesimo(i));
+			} else if (lista.iesimo(i).getE2() > lista.iesimo(ipivot).getE2()) {
+				mayor.agregar(lista.iesimo(i));
+			}
+		}
+		ListaTuplas<String, Integer> concat = new ListaTuplas<String, Integer>();
+
+		return concat.concat(quicksort(menor), pivot, quicksort(mayor));
 	}
 
 	@Override
