a7b1e934c7dfe4384a8dc180ec3fcb8f380e2b03
alef.herrera <alef.herrera@gmail.com>
2014-04-14 17:08:41 +0000

    cambio en el caso de que caiga de una escalera a serpiente o viceversa

--- TP1/src/juego/Elementos.java
+++ TP1/src/juego/Elementos.java
@@ -4,21 +4,23 @@ import code.ListaTuplas;
 import code.Tupla;
 
 public class Elementos {
-	private ListaTuplas<Integer,Integer> grupo;
-	
-	public Elementos()
-	{
-		grupo = new ListaTuplas<Integer,Integer>();
+	private ListaTuplas<Integer, Integer> grupo;
+
+	public Elementos() {
+		grupo = new ListaTuplas<Integer, Integer>();
 	}
-	
-	public void agregar(Integer i,Integer j)
-	{
-		grupo.agregar(new Tupla<Integer,Integer>(i,j));
+
+	public void agregar(Integer i, Integer j) {
+		grupo.agregar(new Tupla<Integer, Integer>(i, j));
 	}
-	
-	public Integer moverse(Integer i)
-	{
-		Integer aux = grupo.buscar(i);
-		return aux == null ? i : aux;
-	}	
+
+	public Integer moverse(Integer i) {
+		Integer nuevapos = grupo.buscar(i);
+		Integer posant = i;
+		while (nuevapos != null) {
+			posant = nuevapos;
+			nuevapos = grupo.buscar(nuevapos);
+		}
+		return nuevapos == null ? posant : nuevapos;
+	}
 }
